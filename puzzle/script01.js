bfaf298d1af454ce78af20f71e2bf491bdb307fd
runtoself <161694236+runtoself@users.noreply.github.com>
2024-03-31 23:25:15 +0800

    puzzle

---
+++ puzzle/script01.js
@@ -0,0 +1,20 @@
+function MD(){
+  prompt("[回答错误] 看来你是麦门信徒，但不妨碍我吐槽麦麦脆汁鸡鸡胸很柴，鸡腿太小没有满足感，面糊很厚浸满了油，麦辣鸡腿堡播到没有肉感，欢迎在下方反驳！")
+  alert("不接受反驳，谢谢！")
+}
+function KFC(){
+  prompt("[回答错误] KFC品控越来越差，炸鸡咸死，薯条发苦，尤其定西街那家")
+  alert("不接受反驳，谢谢！")
+}
+function DICOS(){
+  window.open("DicosYYDS.html");
+  alert(window.location);
+}
+function TJ() {
+    var inputText = document.getElementById('others').value;
+    if (inputText === "华莱士") {
+        alert("看来你是有华莱士喷射抗体啊！");
+    } else {
+        alert("真的，尝尝德克士吧！");
+    }
+}
